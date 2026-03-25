6ffdfcc92bdd20b896827d869a2ebbb46d950e43
impressivecoder1 <rohanrahman251@gmail.com>
2026-03-26 01:29:28 +0600

    footer navbar added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <p>Footer</p>
-        </div>
+        <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+  </aside>
+</footer>
     );
 };
 
--- src/Page/ErrorPage/Errorpage.jsx
+++ src/Page/ErrorPage/Errorpage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+// import { useRouteError } from 'react-router';
 
 const Errorpage = () => {
+    // const error = useRouteError()
     return (
         <div>
-            <p>Not found</p>
+            <p className='text-5xl'>Not found</p>
+            
+            {/* <p>{error.message}h</p> */}
         </div>
     );
 };
