22a48bac47ac75f2f71dc72022cd9c14eb73e8f9
impressivecoder1 <rohanrahman251@gmail.com>
2026-03-26 01:12:09 +0600

    error page added successfully

--- src/Page/ErrorPage/Errorpage.jsx
+++ src/Page/ErrorPage/Errorpage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Errorpage = () => {
     return (
         <div>
-            
+            <p>Not found</p>
         </div>
     );
 };
--- src/Page/Routes/Routes.jsx
+++ src/Page/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../Home/Home";
 import Layouts from "../Layouts/Layouts";
+import Errorpage from "../ErrorPage/Errorpage";
 
  const router = createBrowserRouter([
   {
     path: "/",
     Component: Layouts,
+    errorElement: <Errorpage></Errorpage>,
     children:[
         {   index: true,
            
