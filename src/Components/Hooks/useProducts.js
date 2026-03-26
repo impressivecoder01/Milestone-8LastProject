01c105bc473e0c7fde3d2c87c2db5c9f10baed85
impressivecoder1 <rohanrahman251@gmail.com>
2026-03-27 01:37:01 +0600

    hook made and use

---
+++ src/Components/Hooks/useProducts.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const useProducts = () => {
+const [products, setProducts] = useState([])
+const [loading, setLoading] = useState(true)
+const [error, setError] = useState(null)
+useEffect(()=> {
+    axios(`furnitureData.json`) .then(data => console.log(data.data))
+}, [])
+return {products, loading, error}
+}
+export default useProducts
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router';
 import Product from '../Product/Product';
+import useProducts from '../../Components/Hooks/useProducts';
 
 const Home = () => {
     const products = useLoaderData()
+    const data = useProducts()
+    console.log(data)
     const featuredProducts = products.slice(0,6)
-    console.log(products)
+    // console.log(products)
     return (
         <div>
             <h1 className='text-5xl'>FeaturedProducts</h1>
             <Link className='btn btn-outline ' to={`/allProducts`}>See All </Link>
-        <div className='grid grid-cols-1 md:grid-cols-2 pt-2 lg:grid-cols-3 gap-5'>
+        <div className='grid grid-cols-1 md:grid-cols-2 pt-2  lg:grid-cols-3 gap-5'>
             {
                 featuredProducts.map(product => <Product key={product.id} product={product}></Product>)
             }
