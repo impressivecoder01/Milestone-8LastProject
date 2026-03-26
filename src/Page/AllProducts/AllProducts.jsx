7f328ad2e270131d2a3aed9ed9cd289835983dad
impressivecoder1 <rohanrahman251@gmail.com>
2026-03-27 04:08:41 +0600

    search function  added

--- src/Page/AllProducts/AllProducts.jsx
+++ src/Page/AllProducts/AllProducts.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useProducts from '../../Components/Hooks/useProducts';
 import ShowAllProduct from './ShowAllProduct';
 
 const AllProducts = () => {
     const { error, loading, products } = useProducts()
-    console.log(products)
+    const [search , setSearch] = useState('')
+    const term = search.trim().toLocaleLowerCase()
+    console.log(search)
+    const searchedProducts = term? products.filter(product => product.name.toLocaleLowerCase().includes(term)) : products
+    console.log(searchedProducts)
     return (
         <div>
-            <h1 className='font-semibold my-3'>All Products: {products.length}</h1>
+            <h1 className='font-semibold my-3'>All Products: {searchedProducts.length}</h1>
             <label className="input">
                 <span className="label"></span>
-                <input type="search" placeholder="Search here" />
+                <input value={search} onChange={(e)=> setSearch(e.target.value) } type="search" placeholder="Search here" />
             </label>
 
             <div className='grid grid-cols-1 md:grid-cols-2 pt-2  lg:grid-cols-3 gap-5'>
                 {
-                    products.map(product => <ShowAllProduct product={product} key={product.id}></ShowAllProduct>)
+                    searchedProducts.map(product => <ShowAllProduct product={product} key={product.id}></ShowAllProduct>)
                 }
             </div>
         </div>
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     // console.log(products)
     return (
         <div>
-            <h1 className='text-5xl'>FeaturedProducts</h1>
+            <h1 className='text-5xl'>FeaturedProducts: {products.length}</h1>
             <Link className='btn btn-outline ' to={`/allProducts`}>See All </Link>
         <div className='grid grid-cols-1 md:grid-cols-2 pt-2  lg:grid-cols-3 gap-5'>
             {
